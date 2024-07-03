80a7783e1861386087fca3abddc0eeea30028c7a
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-07-02 19:37:34 -0500

    second commit ejercicios

--- src/tercer/trabajo/PrimerEje.java
+++ src/tercer/trabajo/PrimerEje.java
@@ -8,16 +8,16 @@ public class PrimerEje {
         Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("ingrese el numero");
-        int n=textoScanner.nextInt();
+        int numero=textoScanner.nextInt();
         
-        int cont = 1;
-        if(n>0){
-            for(int i=1;i<=n;i++){
+        int contador = 1;
+        if(numero>0){
+            for(int i=1;i<=numero;i++){
             
-                cont=cont*i;            
+                contador=contador*i;            
             }
             
-            System.out.println("el factorial del numero es "+cont);
+            System.out.println("el factorial del numero es "+contador);
         }else{
             System.out.println("a numero negativo no se le saca afctorial");
         }
