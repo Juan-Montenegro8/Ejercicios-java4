a543ccba123b89b571e77a9a7c203156d63728cc
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-07-04 21:43:28 -0500

    third commit ejercicio

--- src/tercer/trabajo/TercerEje.java
+++ src/tercer/trabajo/TercerEje.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class TercerEje {
 
     public static void main(String[] args) {
-        Scanner t=new Scanner(System.in);
+        Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("ingrese el numero");
-        double n=t.nextDouble();
+        double n=textoScanner.nextDouble();
         
         double cont=0;
         int sw=1;
