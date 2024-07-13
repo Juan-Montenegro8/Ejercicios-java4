55b13139fae978e9256d2f017ee719dbcacee6b5
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-07-12 21:35:11 -0500

    seventh commit ejercicio

--- src/tercer/trabajo/OnceavoTrabajo.java
+++ src/tercer/trabajo/OnceavoTrabajo.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class OnceavoTrabajo {
 
     public static void main(String[] args) {
-        Scanner t=new Scanner(System.in);
+        Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("ingrese el numero");
-        double n=t.nextDouble();
+        double n=textoScanner.nextDouble();
         
                
         for (int i = 1; i <=n; i++) {
