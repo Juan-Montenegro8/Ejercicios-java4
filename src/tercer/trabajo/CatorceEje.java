8fc4a155602898ba2146655984b1e285b981a8f6
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-07-22 20:48:10 -0500

    eighth commit ejercicios

--- src/tercer/trabajo/CatorceEje.java
+++ src/tercer/trabajo/CatorceEje.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class CatorceEje {
 
     public static void main(String[] args) {
-        Scanner t=new Scanner(System.in);
+        Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("ingrese el numero");
-        int n=t.nextInt();
+        int n=textoScanner.nextInt();
         
         int in=0;
         if(n>0){
