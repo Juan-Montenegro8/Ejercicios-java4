70b0e73a8ca73731bf75ad536b594e5504fecf21
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-07-24 20:47:41 -0500

    nineth commit ejercicios

--- src/tercer/trabajo/DieciseisavoEje.java
+++ src/tercer/trabajo/DieciseisavoEje.java
@@ -8,16 +8,16 @@ public class DieciseisavoEje {
         Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("ingrese el numero");
-        int n=textoScanner.nextInt();
+        int numero=textoScanner.nextInt();
         
-        int cont = 0 , con =1 , co=1;
-        if(n>0){
-            for(int i=0;i<=n;i++){
+        int contador0 = 0 , contador1 =1 , contador2=1;
+        if(numero>0){
+            for(int i=0;i<=numero;i++){
             
-                System.out.println(co);
-                co=cont+con;                            
-                cont=con;
-                con=co;
+                System.out.println(contador2);
+                contador2=contador0+contador1;                            
+                contador0=contador1;
+                contador1=contador2;
             }                        
         }else{
             System.out.println("numero superior a 0");
