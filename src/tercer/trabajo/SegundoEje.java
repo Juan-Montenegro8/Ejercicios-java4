f13a34480fdb7392f62e1180af5469997acd2e06
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-13 20:38:30 -0500

    firts commit scanner

--- src/tercer/trabajo/SegundoEje.java
+++ src/tercer/trabajo/SegundoEje.java
@@ -5,24 +5,27 @@ import java.util.Scanner;
 public class SegundoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero=textoScanner.nextInt();
         
-        int contador = 0;
-        if(numero>=0){
-            for(int i=1;i<=numero;i++){
+            int contador = 0;
+            if(numero>=0){
+                for(int i=1;i<=numero;i++){
             
-                contador=contador+i;            
-            }
-        }else{
-            for(int i=-1;i>=numero;i--){
+                    contador=contador+i;            
+                }
+            }else{
+                for(int i=-1;i>=numero;i--){
             
-                contador=contador-i;            
+                    contador=contador-i;            
+                }
             }
+            System.out.println("la sumatoria del numero "+numero+" es "+contador);
+        } catch (Exception e) {
+                System.out.println("Ocurrio un error "+e.getMessage());
         }
-        System.out.println("la sumatoria del numero "+numero+" es "+contador);
     }
     
 }
