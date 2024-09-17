d6a7a6ec3bb0d5c5793973ea077def0eb9cb194d
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-16 19:56:27 -0500

    Firt commit scanner

--- src/tercer/trabajo/TercerEje.java
+++ src/tercer/trabajo/TercerEje.java
@@ -5,26 +5,29 @@ import java.util.Scanner;
 public class TercerEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        double numero=textoScanner.nextDouble();
-        
-        double contador=0;
-        int cambio=1;
-        if(numero>1){
-            for(double i=1;i<=numero;i+=2){
-                if(cambio==1){
-                    contador=contador+1.0/i;   
-                    cambio=0;
-                }else{
-                    contador=contador-1.0/i;   
-                    cambio=1;
-                }            
-            }
-            System.out.println(contador);
-        }else{
-            System.out.println("el numero tiene que ser mayor a 1");
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            double numero=textoScanner.nextDouble();
+            
+            double contador=0;
+            int cambio=1;
+            if(numero>1){
+                for(double i=1;i<=numero;i+=2){
+                    if(cambio==1){
+                        contador=contador+1.0/i;   
+                        cambio=0;
+                    }else{
+                        contador=contador-1.0/i;   
+                        cambio=1;
+                    }            
+                }
+                System.out.println(contador);
+            }else{
+                System.out.println("el numero tiene que ser mayor a 1");
+            }  
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
