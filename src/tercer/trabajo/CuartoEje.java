a96a2294a06423574eb3a971b4b169f5cacd89b6
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-16 19:58:07 -0500

    Firts commit scanner

--- src/tercer/trabajo/CuartoEje.java
+++ src/tercer/trabajo/CuartoEje.java
@@ -5,21 +5,24 @@ import java.util.Scanner;
 public class CuartoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        double numero=textoScanner.nextDouble();
-        double contador1=0,contador2=2;
-        do{
-            contador1=numero%contador2;
-            contador2++;
-        }while(contador1!=0 && contador2<numero);
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            double numero=textoScanner.nextDouble();
+            double contador1=0,contador2=2;
+            do{
+                contador1=numero%contador2;
+                contador2++;
+            }while(contador1!=0 && contador2<numero);
         
         
-        if(contador1==0){
-            System.out.println("no es primo");
-        }else{
-            System.out.println("es primo");
+            if(contador1==0){
+                System.out.println("no es primo");
+            }else{
+                System.out.println("es primo");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
             
     }
