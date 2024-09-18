0c09853094fc24593432eca0973aec19f1efc662
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-17 20:10:14 -0500

    seconds commit scanner

--- src/tercer/trabajo/SextoEje.java
+++ src/tercer/trabajo/SextoEje.java
@@ -5,24 +5,27 @@ import java.util.Scanner;
 public class SextoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero1=textoScanner.nextInt();
-        System.out.println("ingrese numero divisor");
-        int numero2=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero1=textoScanner.nextInt();
+            System.out.println("ingrese numero divisor");
+            int numero2=textoScanner.nextInt();
         
-        int division =0;
+            int division =0;
         
-        if(numero2<numero1 || numero2<=0 || numero1<=-1){
-            System.out.println("no se puede dividir");
-        }else{
-            while(numero1>numero2){
-                numero1=numero1-numero2;
-                division++;
-            }
+            if(numero2<numero1 || numero2<=0 || numero1<=-1){
+                System.out.println("no se puede dividir");
+            }else{
+                while(numero1>numero2){
+                    numero1=numero1-numero2;
+                    division++;
+                }
             
-            System.out.println("la division es "+division+" y el residuo es "+numero1);
+                System.out.println("la division es "+division+" y el residuo es "+numero1);
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
