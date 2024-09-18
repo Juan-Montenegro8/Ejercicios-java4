7d7d27d9edc79d0833af7b28db0f1c55ba1fa6dc
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-17 20:08:56 -0500

    second commit scanner

--- src/tercer/trabajo/QuintoEje.java
+++ src/tercer/trabajo/QuintoEje.java
@@ -5,20 +5,23 @@ import java.util.Scanner;
 public class QuintoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero=textoScanner.nextInt();
         
-        int invertido=0;
+            int invertido=0;
         
-        while(numero>0){
-            int contenedor =numero%10;
-            invertido=invertido*10+contenedor;
-            numero/=10;
-            System.out.println(invertido);
+            while(numero>0){
+                int contenedor =numero%10;
+                invertido=invertido*10+contenedor;
+                numero/=10;
+                System.out.println(invertido);
+            }
+            System.out.println("invertido es "+invertido);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
-        System.out.println("invertido es "+invertido);
     }
     
 }
