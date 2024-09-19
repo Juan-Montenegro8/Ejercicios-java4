61c2ea4577fe9a42178f3c870f113e52720fcafe
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-18 20:12:06 -0500

    Seconds commit scanner

--- src/tercer/trabajo/SeptimoEje.java
+++ src/tercer/trabajo/SeptimoEje.java
@@ -5,20 +5,23 @@ import java.util.Scanner;
 public class SeptimoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero1=textoScanner.nextInt();
-        System.out.println("ingrese numero multiplicador");
-        int numero2=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero1=textoScanner.nextInt();
+            System.out.println("ingrese numero multiplicador");
+            int numero2=textoScanner.nextInt();
         
-        int contador=numero1;
+            int contador=numero1;
                 
-        for(int i=1;i<numero2;i++){
-            numero1+=contador;
+            for(int i=1;i<numero2;i++){
+                numero1+=contador;
+            }
+
+            System.out.println("la multiplicacion es "+numero1);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error "+e.getMessage());
         }
-            
-        System.out.println("la multiplicacion es "+numero1);
         
     }
     
