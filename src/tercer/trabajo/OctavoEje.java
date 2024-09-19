33b17f4d9949c52044111d75445956e53e7dd7d7
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-18 20:14:28 -0500

    Second commit scanner

--- src/tercer/trabajo/OctavoEje.java
+++ src/tercer/trabajo/OctavoEje.java
@@ -5,43 +5,46 @@ import java.util.Scanner;
 public class OctavoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero1=textoScanner.nextInt();
-        System.out.println("ingrese numero multiplicador");
-        int numero2=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero1=textoScanner.nextInt();
+            System.out.println("ingrese numero multiplicador");
+            int numero2=textoScanner.nextInt();
         
-        int contador=0;
+            int contador=0;
                 
-        if(numero1>numero2){
-            contador=numero1%numero2;
-            if(contador==0){
-                System.out.println("el m.c.d. es "+numero2);
-            }
-            while(contador==0){
-                contador=numero2%contador;
-            }
-            if(contador==0){
-                System.out.println("el m.c.d. es "+contador);
-            }
-        }else{
-            if(numero2>numero1){
-               contador=numero2%numero1;
-            if(contador==0){
-                System.out.println("el m.c.d. es "+numero1);
-            }
-            while(contador==0){
-                contador=numero1%contador;
-            }
-            if(contador==0){
-                System.out.println("el m.c.d. es "+contador);
-            } 
+            if(numero1>numero2){
+                contador=numero1%numero2;
+                if(contador==0){
+                    System.out.println("el m.c.d. es "+numero2);
+                }
+                while(contador==0){
+                    contador=numero2%contador;
+                }
+                if(contador==0){
+                    System.out.println("el m.c.d. es "+contador);
+                }
             }else{
-                System.out.println("los numeros son iguales");
+                if(numero2>numero1){
+                   contador=numero2%numero1;
+                if(contador==0){
+                    System.out.println("el m.c.d. es "+numero1);
+                }
+                while(contador==0){
+                    contador=numero1%contador;
+                }
+                if(contador==0){
+                    System.out.println("el m.c.d. es "+contador);
+                } 
+                }else{
+                    System.out.println("los numeros son iguales");
+                }
             }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
-        
+
     }
     
 }
