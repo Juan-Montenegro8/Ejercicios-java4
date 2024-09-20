99cc8f5ed1260bf179a65a68fde626471d79011b
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-19 21:52:05 -0500

    third commit scanner

--- src/tercer/trabajo/NovenoEje.java
+++ src/tercer/trabajo/NovenoEje.java
@@ -5,30 +5,33 @@ import java.util.Scanner;
 public class NovenoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese la base");
-        int base=textoScanner.nextInt();
-        System.out.println("ingrese exponente");
-        int exponente=textoScanner.nextInt();
-        
-        int contador=1;
-        
-        if(exponente>=0){
-        
-            for (int i = 1; i <=exponente; i++) {
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese la base");
+            int base=textoScanner.nextInt();
+            System.out.println("ingrese exponente");
+            int exponente=textoScanner.nextInt();
             
-                contador*=base;
+            int contador=1;
+        
+            if(exponente>=0){
+        
+                for (int i = 1; i <=exponente; i++) {
             
+                    contador*=base;
+            
+                }
+                System.out.println("el resdulatdo es "+contador);
+            }else{
+                for (int i = -1; i >=exponente; i--) {
+            
+                    contador*=base;
+                
+                }
+                System.out.println("el resdulatdo es 1/"+contador);
             }
-            System.out.println("el resdulatdo es "+contador);
-        }else{
-            for (int i = -1; i >=exponente; i--) {
-            
-                contador*=base;
-            
-            }
-            System.out.println("el resdulatdo es 1/"+contador);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
