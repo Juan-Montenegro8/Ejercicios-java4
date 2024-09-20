d56a358ee968949d2df84a641631eb9903dc89e4
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-20 14:11:45 -0500

    Third commit scanner

--- src/tercer/trabajo/DoceavoEje.java
+++ src/tercer/trabajo/DoceavoEje.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 public class DoceavoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero=textoScanner.nextInt();
         
-        int contador1 = 1;
-        int contador2 = 0;
-        if(numero>0){
-            for(int i=1;i<=numero;i++){
+            int contador1 = 1;
+            int contador2 = 0;
+            if(numero>0){
+                for(int i=1;i<=numero;i++){
             
-                contador1=contador1*i;            
-                contador2+=contador1;
+                    contador1=contador1*i;            
+                    contador2+=contador1;
+                } 
+                System.out.println("la sumatoria es "+contador2);
+            }else{
+                System.out.println("a numero negativo no se le saca afctorial");
             } 
-            System.out.println("la sumatoria es "+contador2);
-        }else{
-            System.out.println("a numero negativo no se le saca afctorial");
-        } 
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
