3b83b73821e42fdba334f23ab874d4afa8a24779
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-20 14:09:18 -0500

    Thirds commit scanner

--- src/tercer/trabajo/OnceavoTrabajo.java
+++ src/tercer/trabajo/OnceavoTrabajo.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 public class OnceavoTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        double numero=textoScanner.nextDouble();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            double numero=textoScanner.nextDouble();
         
                
-        for (int i = 1; i <=numero; i++) {
-        double contador1=0,contador2=2;    
-            do{
-                contador1=i%contador2;
-                contador2++;
-            }while(contador1!=0 && contador2<i);
-            if(contador1!=0){
-                System.out.println(i+" es primo");
+            for (int i = 1; i <=numero; i++) {
+            double contador1=0,contador2=2;    
+                do{
+                    contador1=i%contador2;
+                    contador2++;
+                }while(contador1!=0 && contador2<i);
+                if(contador1!=0){
+                    System.out.println(i+" es primo");
+                }
             }
-        }             
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());    
+        }
         
     }
     
