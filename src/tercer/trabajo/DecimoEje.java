0bdbfa00f0739e05a0b9f1309544c52d6f6709e4
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-19 21:53:25 -0500

    thirds commit scanner

--- src/tercer/trabajo/DecimoEje.java
+++ src/tercer/trabajo/DecimoEje.java
@@ -5,21 +5,24 @@ import java.util.Scanner;
 public class DecimoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero=textoScanner.nextInt();
         
-        int contador = 1;
-        if(numero>0){
-            for(int i=1;i<=numero;i++){
+            int contador = 1;
+            if(numero>0){
+                for(int i=1;i<=numero;i++){
             
-                contador=contador*i;            
-                System.out.println("el factorial del numero "+i+" es "+contador);
-            }                        
-        }else{
-            System.out.println("a numero negativo no se le saca afctorial");
-        }          
+                    contador=contador*i;            
+                    System.out.println("el factorial del numero "+i+" es "+contador);
+                }                        
+            }else{
+                System.out.println("a numero negativo no se le saca afctorial");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
