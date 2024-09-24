463bd5c7a0319752cbd20c8147242e202e697fa3
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-23 20:29:27 -0500

    Fourth commit scanner

--- src/tercer/trabajo/TreceaboEje.java
+++ src/tercer/trabajo/TreceaboEje.java
@@ -5,25 +5,28 @@ import java.util.Scanner;
 public class TreceaboEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        double numero1=textoScanner.nextDouble();
-        System.out.println("ingrese el numero");
-        double numero2=textoScanner.nextDouble();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            double numero1=textoScanner.nextDouble();
+            System.out.println("ingrese el numero");
+            double numero2=textoScanner.nextDouble();
                 
-        double contador1 =1;
-        double contador2 =0;
+            double contador1 =1;
+            double contador2 =0;
         
-        if(numero2>0 && numero1>0){
-            for (int i = 1; i <=numero2; i++) {
-                contador1=contador1*i;
-                contador2+=numero1/contador1;
+            if(numero2>0 && numero1>0){
+                for (int i = 1; i <=numero2; i++) {
+                    contador1=contador1*i;
+                    contador2+=numero1/contador1;
+                }
+                double resultado=contador2+1;
+                System.out.println("el resultado es "+resultado);
+            }else{
+                System.out.println("no pueden ir numeros  negativos ni el 0");
             }
-            double resultado=contador2+1;
-            System.out.println("el resultado es "+resultado);
-        }else{
-            System.out.println("no pueden ir numeros  negativos ni el 0");
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error"+e.getMessage());
         }
     }
     
