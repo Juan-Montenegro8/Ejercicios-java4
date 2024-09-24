93f5b89a9ea6d57317f8711d85877c530c756b75
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-23 20:31:18 -0500

    Fourths commit scanner

--- src/tercer/trabajo/CatorceEje.java
+++ src/tercer/trabajo/CatorceEje.java
@@ -5,21 +5,24 @@ import java.util.Scanner;
 public class CatorceEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero=textoScanner.nextInt();
         
-        int invertido=0;
-        if(numero>0){
-        while(numero>0){
-            int resultado =numero%10;
-            numero/=10;
-            invertido+=resultado;
-        }
-        System.out.println("invertido es "+invertido);
-        }else{
-            System.out.println("numeros superiores a 0");
+            int invertido=0;
+            if(numero>0){
+            while(numero>0){
+                int resultado =numero%10;
+                numero/=10;
+                invertido+=resultado;
+            }
+            System.out.println("invertido es "+invertido);
+            }else{
+                System.out.println("numeros superiores a 0");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
