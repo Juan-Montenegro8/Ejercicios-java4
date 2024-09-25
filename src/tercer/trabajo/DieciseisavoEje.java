fe3437c4e1ba5cc3f3307a52202f9467ec478530
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-24 20:19:45 -0500

    Fourth commit scanner

--- src/tercer/trabajo/DieciseisavoEje.java
+++ src/tercer/trabajo/DieciseisavoEje.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 public class DieciseisavoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero=textoScanner.nextInt();
         
-        int contador0 = 0 , contador1 =1 , contador2=1;
-        if(numero>0){
-            for(int i=0;i<=numero;i++){
-            
-                System.out.println(contador2);
-                contador2=contador0+contador1;                            
-                contador0=contador1;
-                contador1=contador2;
-            }                        
-        }else{
-            System.out.println("numero superior a 0");
+            int contador0 = 0 , contador1 =1 , contador2=1;
+            if(numero>0){
+                for(int i=0;i<=numero;i++){
+                
+                    System.out.println(contador2);
+                    contador2=contador0+contador1;                            
+                    contador0=contador1;
+                    contador1=contador2;
+                }                        
+            }else{
+                System.out.println("numero superior a 0");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error:  "+e.getMessage());
         }
     }
     
