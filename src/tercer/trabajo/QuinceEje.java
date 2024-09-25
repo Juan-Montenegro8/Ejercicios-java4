710e335d167f1598523ec863374f8723157b0065
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-24 20:17:05 -0500

    fourths commit scanner

--- src/tercer/trabajo/QuinceEje.java
+++ src/tercer/trabajo/QuinceEje.java
@@ -5,26 +5,29 @@ import java.util.Scanner;
 public class QuinceEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int numero=textoScanner.nextInt();
         
-        int invertido=0;
-        if(numero>0){
-        while(numero>0){
-            int resultado =numero%10;
-            numero/=10;
-            int contador=0;
-            for(int i=1;i<=resultado;i++){
+            int invertido=0;
+            if(numero>0){
+                while(numero>0){
+                    int resultado =numero%10;
+                    numero/=10;
+                    int contador=0;
+                    for(int i=1;i<=resultado;i++){
             
-                contador=contador+i;            
+                        contador=contador+i;            
+                    }
+                    invertido+=contador;
+                }
+                System.out.println("invertido es "+invertido);
+            }else{
+                System.out.println("numeros superiores a 0");
             }
-            invertido+=contador;
-        }
-        System.out.println("invertido es "+invertido);
-        }else{
-            System.out.println("numeros superiores a 0");
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
