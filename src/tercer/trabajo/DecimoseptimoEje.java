2ff8db559afa387d44669bfa83e349f4db665447
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-25 09:42:02 -0500

    Fifth commit scanner

--- src/tercer/trabajo/DecimoseptimoEje.java
+++ src/tercer/trabajo/DecimoseptimoEje.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class DecimoseptimoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int number=textoScanner.nextInt();
         
-        System.out.println("los primeros "+number+" numeros cuadrados son");
-        for (int i = 1; i <=number; i++) {
-            int count=i*i;
-            System.out.println(i+" x "+i+" = "+count);
+        
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese el numero");
+            int number=textoScanner.nextInt();
+        
+            System.out.println("los primeros "+number+" numeros cuadrados son");
+            for (int i = 1; i <=number; i++) {
+                int count=i*i;
+                System.out.println(i+" x "+i+" = "+count);
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
